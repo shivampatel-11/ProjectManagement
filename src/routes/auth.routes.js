49c37ae690a78fa03ae7fa21ddcd747416693c45
Patel Shivam <hello.patelshivam@gmail.com>
2026-08-20 21:04:32 +0530

    validation part done

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import {registerUser} from "../controllers/auth.controllers.js"
+import { validate } from "../middleware/validator.middleware.js";
+import { userRegisterValidator } from "../validators/index.js";
 
 
 const router = Router();
 
 
-router.route("/register").post(registerUser)
+router.route("/register").post( userRegisterValidator(), validate, registerUser)
 
 
 export default router;
